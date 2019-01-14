f9226df841abfb2cc89b2922ad710767df330758
mqp0713 <309729529@qq.com>
2019-01-14 17:58:50 +0800

    feat: 修改路径

---
+++ build/precache-manifest.b76462dfd188f09fa8dd39def5d15ff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dfa0d39a35cb33c2ae4",
+    "url": "./static/css/main.7bee2859.chunk.css"
+  },
+  {
+    "revision": "6dfa0d39a35cb33c2ae4",
+    "url": "./static/js/main.6dfa0d39.chunk.js"
+  },
+  {
+    "revision": "5fb161e1637aa45e9c43",
+    "url": "./static/js/1.5fb161e1.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2d515270f40a1db1e7f91a9317b8401c",
+    "url": "./index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0da64aa182844b672e3fae9cc94d4e8a.js"
+  "./precache-manifest.b76462dfd188f09fa8dd39def5d15ff2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
