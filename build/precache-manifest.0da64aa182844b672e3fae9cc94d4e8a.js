154dee85abc783d8ab2c61f3e7a174c761b97d02
mqp0713 <309729529@qq.com>
2019-01-14 17:24:31 +0800

    feat: npm run build

---
+++ build/precache-manifest.0da64aa182844b672e3fae9cc94d4e8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "96b793e0ed79604f3967",
+    "url": "/static/css/main.7bee2859.chunk.css"
+  },
+  {
+    "revision": "96b793e0ed79604f3967",
+    "url": "/static/js/main.96b793e0.chunk.js"
+  },
+  {
+    "revision": "5fb161e1637aa45e9c43",
+    "url": "/static/js/1.5fb161e1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fad632216d05a3fa11126f9d0b3b06e4",
+    "url": "/index.html"
+  }
+];
